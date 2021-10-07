2d929bb3c965d7b69e9721913988c69067671fac
herojia <hodalaplus@gmail.com>
2021-10-07 19:20:45 +0800

    建立頁面模版

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,9 @@ const express = require("express")
 const router = express.Router()
 
 const home = require("./modules/home")
+const short = require("./modules/short")
 
 router.use("/", home)
+router.use("/short", short)
 
 module.exports = router
